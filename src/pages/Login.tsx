9eba0a4799a8d757c667890d19acf05abc055963
Sandeep Mandal <sandeepmandal0504@gmail.com>
2024-08-25 23:58:54 +0530

    Implementing ecommerce frontend.

---
+++ src/pages/Login.tsx
@@ -0,0 +1,48 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { FcGoogle } from "react-icons/fc"
+import { auth } from "../firebase"
+
+const Login = () => {
+    const [gender, setGender] = useState<string>("")
+    const [date, setDate] = useState<string>("")
+
+    const loginHandler = async( ) => {
+        try {
+            const provider = new GoogleAuthProvider();
+            const { user } = await signInWithPopup( auth, provider );
+            
+        } catch ( error ) {
+            toast.error( "Sign In Fail" );
+        }
+    }
+
+  return (
+    <div className="login">
+        <main>
+            <h1 className="heading">Login</h1>
+            <div>
+                <label>Gender</label>
+                <select value={gender} onChange={(e) => setGender(e.target.value)}>
+                    <option value="">Select Your Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+            </div>
+            <div>
+                <label>Date of birth</label>
+                <input type="date" value={date} onChange={(e) => setDate( e.target.value )}/>
+            </div>
+            <div>
+                <p>Already Signed In Once</p>
+                <button onClick={loginHandler}>
+                    <FcGoogle/><span>Sign in with Google</span>
+                </button>
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default Login
